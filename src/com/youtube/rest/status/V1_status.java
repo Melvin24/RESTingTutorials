40c3b677b4b5695ad05b77f38f9ad5386ba6fe12
melvin <09mejohn@gmail.com>
2015-07-31 10:05:59 +0100

    A new commit on the 31/07/15 10.05

---
+++ src/com/youtube/rest/status/V1_status.java
@@ -0,0 +1,20 @@
+package com.youtube.rest.status;
+
+//class used to return the health of this API -> com.youtube.rest
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/status")//this is the root path
+public class V1_status {
+
+	//first method is going to return the title 
+	//"@" is used to tell jersey what to look for and how to handle request, basically how the restfult webservice know what it needs to do
+	//@GET is a GET HTTP Call
+	//@Produce is the format of the product, how it is going to be encoded
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle(){
+		return "<h1>Java Web Service</h1>";
+	}
+}
